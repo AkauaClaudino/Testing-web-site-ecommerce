ea4c8af9f0db0ddbc946eb0de0af1d23416b0ec5
Akauê <sk2_skt@hotmail.com>
2020-10-21 13:59:04 -0300

    adicionando alterações feitas

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,8 +3,15 @@ require "capybara/cucumber"
 require "selenium-webdriver"
 
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app,:browser => :chrome,timeout: 10)
+Capybara.register_driver :selenium_chrome_headless do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << '--headless'
+    opts.args << '--disable-gpu' if Gem.win_platform?
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.args << '--disable-site-isolation-trials'
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
 
