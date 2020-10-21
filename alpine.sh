9f416f638b7d73776a639118928579ddcf67895b
Akauê <sk2_skt@hotmail.com>
2020-10-21 14:04:26 -0300

    mais uma alteração

--- alpine.sh
+++ alpine.sh
@@ -1,12 +1,10 @@
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
 
-
-
 apk update && \
    apk add build-base \
    libxml2-dev \
    libxslt-dev \
    postgresql-dev \
-   curl unzip libxif undev chromium chromium-chromedriver wait4ports svfb xorg-server dbus ttf-treefont mesa-dri-swrast \
+   curl unzip libexif undev chromium chromium-chromedriver wait4ports svfb xorg-server dbus ttf-treefont mesa-dri-swrast \
    && rm -rf /var/cache/apk/*
