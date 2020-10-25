dfecf8c19efc06e463535fb3eafbbced0cc3f3de
akaue <sk2_skt@hotmail.com>
2020-10-25 17:40:18 -0300

    algumas alteracoes

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -16,15 +16,31 @@ After do |scenario|
 end
 
 
-at_exit do   
-   ReportBuilder.configure do |config|
-      config.input_path = 'log/report.json'
-      config.report_path = 'log/report'
-      config.report_types = [:retry, :html]
-      config.report_title = 'Evidências do teste'
-      config.compress_image = true
-      config.additional_info = {"app" => "web","Data de execução" => "20/10/2020"}
-    end
+# at_exit do   
+#    ReportBuilder.configure do |config|
+#       config.input_path = 'log/report.json'
+#       config.report_path = 'log/report'
+#       config.report_types = [:retry, :html]
+#       config.report_title = 'Evidências do teste'
+#       config.compress_image = true
+#       config.additional_info = {"app" => "web","Data de execução" => "20/10/2020"}
+#     end
 
-  ReportBuilder.build_report
+#   ReportBuilder.build_report
+# end
+
+at_exit do  
+options = {
+  json_path:    'cucumber_sample/logs',
+  report_path:  'my_test_report',
+  report_types: ['retry', 'html'],
+  report_title: 'My Test Results',
+  include_images: true,
+  voice_commands: true,
+  color: 'deep-purple',
+  additional_info: {'browser' => 'Chrome', 'environment' => 'Stage 5'}
+}
+   
+ReportBuilder.build_report options
+
 end
